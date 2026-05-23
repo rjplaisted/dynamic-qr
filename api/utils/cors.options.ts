71b1ad126326e8438911cc1a48e716f3af0159b0
coomico <42692043+coomico@users.noreply.github.com>
2026-05-23 19:38:12 +0700

    format: consistent formatting and (auto) add missing newline

--- api/utils/cors.options.ts
+++ api/utils/cors.options.ts
@@ -1,9 +1,9 @@
-import { CorsOptions } from "cors";
-import { WebUrl } from "./envs";
+import { CorsOptions } from 'cors';
+import { WebUrl } from './envs';
 
 export const corsOptions: CorsOptions = {
   // origin must be `https` for able to transfer cookies
   origin: WebUrl,
   allowedHeaders: 'Content-Type,Authorization',
-  credentials: true
+  credentials: true,
 };
